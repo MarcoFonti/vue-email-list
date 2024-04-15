a3f341ae92e8c9fa237c83deea08337fd65b566d
MarcoFonti <theemporiumg@gmail.com>
2024-04-15 13:50:51 +0200

    add commit

--- js/script.js
+++ js/script.js
@@ -1,38 +1,56 @@
 // CHECK 
 console.log('JS VUE OK', Vue);
 
-// URL BOOLEAN
+/* URL API */
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-// ESTRAPOLO FUNZIONE DA VUE
-const {createApp} = Vue;
 
-// CREO VARIBILE E RICHIAMO LA FUNZIONE
-const app = createApp ({
+/* ESTRAPOLO FUNZIONE DA VUE */
+const { createApp } = Vue;
+
+
+/* CREO VARIBILE E RICHIAMO LA FUNZIONE */
+const app = createApp({
+
+
+    /* NOME PAGINA */
     name: 'VueEmail',
-    // USO DATA CON ALL'INTENRO IL RETURN 
+
+
+    /* DATI DI BASE DA UTILIZZARE */
     data() {
         return {
-            // ARRAY DI LISTA EMAIL
+
+
+            /* ARRAY A CUI AGGIURE LA LISTA EMAIL */
             mails: [],
         }
     },
 
-    // ALL'AVVIO DELLA PAGINA
+
+    /* USO IL METODO CREATED PER FARE UNA CHIAMATA API ALL'AVVIO DELLA PAGINA  */
     created() {
-        // CICLO 10 EMAIL
-        for (let i = 0; i < 10 ; i++) {
-            // AXIOS
-            axios.get(endpoint).then((res)=> {
-                // CONSOLE 
+
+
+        /* CICLO FOR PER RICHIAMRE 10 VOLTE API PER GENERARMI 10 EMAIL DIVERSE */
+        for (let i = 0; i < 10; i++) {
+
+
+            /* AXIOS */
+            axios.get(endpoint).then((res) => {
+
+
+                /* CONTROLLO RIPOSTA IN RES.DATA */
                 console.log(res.data);
-                // PUSH NELL'ARRAY MAILS
+
+
+                /* RICHIAMO IL MIO ARRAY PER AGGIUNGERE A OGNI GIRO UN EMAIL DIVERSA */
                 this.mails.push(res.data.response);
             });
-            
         };
     }
 })
 
-// COLLEGO ID 
+
+/* COLLEGO ID  */
 app.mount('#root');
